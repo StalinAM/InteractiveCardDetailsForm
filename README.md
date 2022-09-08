ad56c3a30a75551378a5fd7892f02a6d0e4edd41
Stalin Acurio <sta_vla411@hotmail.com>
2022-09-08 17:08:49 -0500

    Verison final

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# InteractiveCardDetailsForm
-credit card form design 
+# Frontend Mentor - Interactive card details form solution
+
+This is a solution to the [Interactive card details form challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/interactive-card-details-form-XpS8cKZDWw). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+
+## Table of contents
+
+- [Frontend Mentor - Interactive card details form solution](#frontend-mentor---interactive-card-details-form-solution)
+  - [Table of contents](#table-of-contents)
+    - [Screenshot](#screenshot)
+    - [Links](#links)
+  - [My process](#my-process)
+    - [Built with](#built-with)
+  - [Author](#author)
+
+### Screenshot
+
+Mobile version
+
+![](./images/screenshot3.PNG)
+
+![](./images/screenshot5.PNG)
+
+Desktop version
+
+![](./images/screenshot1.PNG)
+
+![](./images/screenshot4.PNG)
+
+Data validation
+
+![](./images/screenshot2.PNG)
+
+### Links
+
+- Solution URL: [GitHub](https://github.com/StalinAM/InteractiveCardDetailsForm.git)
+- Live Site URL: [StalinAM](https://stalinam.github.io/InteractiveCardDetailsForm/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- CSS Grid
+- CSS Flexbox
+- JS Jquery
+- Mobile-first workflow
+- Responsive web design
+
+## Author
+
+- GitHub - [StalinAM](https://github.com/StalinAM)
+- Frontend Mentor - [@StalinAM](https://www.frontendmentor.io/profile/StalinAM)
--- main.js
+++ main.js
@@ -1,11 +1,39 @@
+function addZeroU(valor) {
+    if (!valor) {
+        return '00/'
+    }
+    if (parseInt(valor) > 9) {
+        return valor + '/'
+    }
+    return `0${parseInt(valor)}/`
+}
+const nameSpan = document.querySelector('.name-span')
+const numberSpan = document.querySelector('.number-span')
+const mmSpan = document.querySelector('.mm-span')
+const yySpan = document.querySelector('.yy-span')
+const cvcSpan = document.querySelector('.cvc-span')
+
 let nameCardInput = document.querySelector('#name-input');
 const nameCard = document.querySelector('#name-card')
 nameCardInput.addEventListener('keyup', () => {
+    if (nameCardInput.value.match(/(\d+)/g)) {
+        nameSpan.innerHTML = 'Letters only'
+    } else {
+        nameSpan.innerHTML = ''
+    }
     nameCard.innerHTML = nameCardInput.value.toUpperCase();
+
 })
 let numberCardInput = document.querySelector('#number-input');
 const numberCard = document.querySelector('#number-card')
 numberCardInput.addEventListener('keyup', () => {
+    if (numberCardInput.value.match(/([a-zA-Z]+)/g)) {
+        numberSpan.innerHTML = 'Numbers only'
+    } else if (numberCardInput.value.length > 16) {
+        numberSpan.innerHTML = 'Max. 16 numbers'
+    } else {
+        numberSpan.innerHTML = ''
+    }
     numberCard.innerHTML = `${numberCardInput.value.substring(0, 4)} ${numberCardInput.value.substring(4, 8)} ${numberCardInput.value.substring(8, 12)} ${numberCardInput.value.substring(12, 16)}`;
 })
 
@@ -14,24 +42,52 @@ let yyCardInput = document.querySelector('#input-yy')
 let mmCard = document.querySelector('#date-card')
 
 mmCardInput.addEventListener('keyup', () => {
-    mmCard.innerHTML = `${mmCardInput.value}/`
+    if (parseInt(mmCardInput.value) > 12) {
+        mmSpan.innerHTML = "Wrong month"
+    } else {
+        mmSpan.innerHTML = ''
+    }
+    mmCard.innerHTML = addZeroU(mmCardInput.value)
 })
 yyCardInput.addEventListener('keyup', () => {
-    mmCard.innerHTML = `${mmCard.textContent.substring(0, 3)}${yyCardInput.value}`
+    if (parseInt(yyCardInput.value) > 99) {
+        yySpan.innerHTML = "Wrong year"
+    } else {
+        yySpan.innerHTML = ''
+    }
+    mmCard.innerHTML = `${mmCard.textContent.substring(0, 3)}${addZeroU(yyCardInput.value).substring(0, 2)}`
+
 })
-let cvcInput=document.querySelector('#cvc-input')
+let cvcInput = document.querySelector('#cvc-input')
 const cvcCard = document.querySelector('#cvc-card')
 
-cvcInput.addEventListener('keyup', ()=>{
-    cvcCard.innerHTML =cvcInput.value
+cvcInput.addEventListener('keyup', () => {
+    if (cvcInput.value.length > 3) {
+        cvcSpan.innerHTML = "Max. 3 numbers"
+    } else {
+        cvcSpan.innerHTML = ''
+    }
+    cvcCard.innerHTML = cvcInput.value
 })
 
-const buttonConfirm=document.querySelector('#submint-data')
-const screnThankYou =document.querySelector('.thank-you-container')
-const screenForm =document.querySelector('.form-container')
+const buttonContinue = document.querySelector('button')
+const screnThankYou = document.querySelector('.thank-you-container')
+const screenForm = document.querySelector('.form-container')
 
-buttonConfirm.addEventListener('click',(e)=>{
+const allSpan = document.querySelectorAll('span')
+const allInput =document.querySelectorAll('input')
+
+screenForm.addEventListener('submit', (e) => {
     e.preventDefault()
-    screnThankYou.classList.toggle('inactive')
-    screenForm.classList.toggle('inactive')
+    for (const item of allSpan) {
+        if (!item.value) {
+            screnThankYou.classList.toggle('inactive')
+            screenForm.classList.toggle('inactive')
+        }
+    }
+})
+buttonContinue.addEventListener('click', () => {
+    
+    location.reload()
+    
 })
